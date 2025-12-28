b9269b230f533410c6b7a05655d2ec6f30b3dc55
bcoconuts <bjburgess96@gmail.com>
2025-12-27 22:05:08 -0600

    Refactor game functions and update variable structures
    Refactor game logic and improve function definitions.

--- main.py
+++ main.py
@@ -4,120 +4,107 @@
 import random
 
 
-QUICK = 1
-EASY = 3
-MEDIUM = 5
-HARD = 7
-
-
-WEAPONS = ["r", "p", "s"]
+GAME_SIZE = {"q": 1, "s": 3, "m": 5, "l": 7} #quick, small, medium, large
+BEST_OF_SIZE = GAME_SIZE * 2 - 1
+WEAPON_NAME = {"r": "Rock", "p": "Paper", "s": "Scissors"}
+WEAPON = ["r", "p", "s"]
 BEATS = {"r": "s", "p": "r", "s": "p"}
-WEAPON_NAMES = {"r": "Rock", "p": "Paper", "s": "Scissors"}
 
 
-def get_valid_response(valid_choices: list, prompt: str, error_msg: str = "Invalid Input") -> str:
-    """Get valid input from user.
-
-    Repeatedly prompts until user enters desired value.
-
-    Args:
-        valid_choices: the choices the user must pick between.
-        prompt: The message displayed to the user.
-        error_msg: Message shown for invalid input.
-
-    Returns:
-        A string: value of string subject to valid_choices argument.
-    """
+def get_valid_response(valid_choices: set[str], prompt: str, error_msg: str = "Invalid Input") -> str:
     while True:
         response = input(prompt).strip().lower()
-        if response not in (valid_choices):
+        if response not in valid_choices:
             print(random.choice(error_msg))
         else:
             return response
 
 
-def _get_clash_result(user_attack: str, cpu_attack: str) -> tuple:
-    """Check which input/attack wins the game.
-
-    Args:
-        prompt: The input message displayed to the user, used to collect the user's weapon.
-        cpu_weapon: CPU's weapon.
-
-    Returns:
-        A tuple: (User win (1, 0), CPU win (0, 1), or Draw (0, 0)).
-    """
-    if user_attack == cpu_attack:
-        print("Draw!")
-        clash_result = (0, 0)
-        return clash_result
-    elif BEATS[user_attack] == cpu_attack:
-        print(f"You Win... {WEAPON_NAMES[user_attack]} beats {WEAPON_NAMES[cpu_attack]}.")
-        clash_result = (1, 0)
-        return clash_result
+def determine_winner(player_attack: str, cpu_attack:str) -> bool:
+    if BEATS[player_attack] == cpu_attack:
+        win = True
+        return win
     else:
-        print(f"I Win!!! {WEAPON_NAMES[cpu_attack]} beats {WEAPON_NAMES[user_attack]}")
-        clash_result = (0, 1)
-        return clash_result
+        win = False
+        return win
 
 
-def get_game_settings() -> int:
-    """User selects what size game they would like to play. Best of X + 2.
+def get_clash_result(player_attacks: dict, cpu_attack: str) -> dict:
+    for key in player_attacks:
+        win = determine_winner(player_attacks[key], cpu_attack)
 
-    Args: n/a
 
-    Returns:
-        An int: Size of game (best of x + 2).
-    """
+def play_new_game_choice() -> bool:
+    response = get_valid_response(["y", "n"], "New Game? [Y]es or [N]o?: ")
+    if response == "y":
+        return True
+    else:
+        return False
+
+
+def get_game_size() -> int:
     while True:
-        difficulty = get_valid_response(["q", "e", "m", "h"], "\n[Q]uick Play? [E]asy? [M]edium? [H]ard?: ")
-        if difficulty == "q":
-            size = QUICK
-            return size
-        elif difficulty == "e":
-            size = EASY
-            return size
-        elif difficulty == "m":
-            size = MEDIUM
-            return size
-        else:
-            size = HARD
-            return size
+        size = get_valid_response(["q", "s", "m", "l"], "\n[Q]uick Play? [S]mall? [M]edium? [L]arge?: ")
+        return GAME_SIZE[size]
 
 
-def play_game_loop(size):
-    record = (0, 0)
-    while record[0] < size and record[1] < size:
-        cpu_attack = random.choice(WEAPONS)
-        user_attack = get_valid_response(WEAPONS, "\nChoose your weapon. [R]ock, [P]aper, or [S]cissors?: ")
-        clash_result = _get_clash_result(user_attack, cpu_attack)
-        record = (record[0] + clash_result[0], record[1] + clash_result[1])
+def get_players() -> int:
+    while True:
+        players = int(get_valid_response(["1", "2", "3"], "How many players are playing against me (CPU)? [1] or [2]?: "))
+        return players
+      
 
-    return record
-
-
-def display_per_game_results(record, size):
-    user_wins = record[0]
-    cpu_wins = record[1]
-    best_of_size = size + 2
-    if user_wins > cpu_wins:
-        print(f"Nice Job. In a best of {best_of_size} round, you beat me {user_wins} times!\n")
-    else:
-        print(f"Better luck next time. In a best of {best_of_size} round, I beat you {cpu_wins} times!\n")
+def set_up_score_sheet(players: int) -> dict:
+    score_sheet = {}
+    for p in range(1, players):
+        score_sheet[f"Player_{p}"] = 0
+    score_sheet["CPU"] = 0
+    return score_sheet
 
 
-def display_total_results(all_user_wins, all_cpu_wins, all_sizes):
-    pass
+def get_player_attacks(players: int) -> dict:
+    player_attacks = {}
+    for p in range(1, players):
+        player_attacks[f"Player_{p}"] = get_valid_response(WEAPON, "\nChoose your weapon. [R]ock, [P]aper, or [S]cissors?: ")
+    return player_attacks
+
+
+def adjust_score_sheet(score_sheet: dict, clash_result: dict) -> dict:
+    for key in clash_result:
+        score_sheet[key] =+ clash_result[key]
+    return score_sheet
+
+
+def play_game_loop(size: int, players: int, score_sheet: dict) -> dict:
+    while any in score_sheet.values() < size:
+        cpu_attack = random.choice(WEAPON)
+        player_attacks = get_player_attacks(players)
+        clash_result = get_clash_result(player_attacks, cpu_attack)
+        score_sheet = adjust_score_sheet(score_sheet, clash_result)
+
+    return score_sheet
+
+
+# def display_per_game_results(record, size):
+#     user_wins = record[0]
+#     cpu_wins = record[1]
+#     best_of_size = size + 2
+#     if user_wins > cpu_wins:
+#         print(f"Nice Job. In a best of {best_of_size} round, you beat me {user_wins} times!\n")
+#     else:
+#         print(f"Better luck next time. In a best of {best_of_size} round, I beat you {cpu_wins} times!\n")
+
+
+# def display_total_results(all_user_wins, all_cpu_wins, all_sizes):
+#     pass
 
 
 def main():
-    while True:
-        response = get_valid_response(["y", "n"], "New Game? [Y]es or [N]o?: ")
-        if response == "y":
-            size = get_game_settings()
-            record = play_game_loop(size)
-            display_per_game_results(record, size)
-        else:
-            break
+    while play_new_game_choice():
+        size = get_game_size()
+        record = play_game_loop(size)
+        print(record)
+        # display_per_game_results(record, size)
 
 if __name__ == "__main__":
     main()
