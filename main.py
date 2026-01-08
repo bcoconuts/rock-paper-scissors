9b18421ea694fa542478d697a29d6b476aca37d1
bcoconuts <bjburgess96@gmail.com>
2026-01-08 00:52:49 -0600

    Refactor game type and new game choice handling

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ import random
 # ================================
 
 MAX_PLAYERS = 3
-GAME_TYPE = {"f": "Free For All", "a": "All vs CPU"}
+NEW_GAME_CHOICES = {
+    "y": {"name": "Yes"}, #yes must appear in first slot
+    "n": {"name": "No"}
+}
+GAME_TYPE = {
+    "f": {"name": "Free For All"}, #default game type must appear in first slot
+    "a": {"name": "All vs CPU"}
+}
 DIFFICULTIES = {
     "q": {"name": "Quick Play", "wins": 1, "best_of": 1},
     "e": {"name": "Easy", "wins": 2, "best_of": 3},
@@ -35,6 +42,32 @@ def get_valid_response(valid_choices: set, prompt: str, error_msg: list = ["Inva
             print(random.choice(error_msg))
         else:
             return response
+
+
+def construct_prompt_ending(keys: list[str]) -> str:
+    keys_with_brackets = [f"[{i[0].upper()}]{i[1:]}" if len(i) > 1 else f"[{i.upper()}]" for i in keys]
+    main_text = ", ".join(keys_with_brackets[:-1])
+    if len(keys_with_brackets) == 2:
+        main_text = main_text.__add__(" or")
+    elif len(keys_with_brackets) > 2:
+        main_text = main_text.__add__(", or")
+    prompt_end = f"{main_text} {keys_with_brackets[-1]}?: "
+    return prompt_end
+
+
+def construct_prompt_and_keys(selection: int | dict) -> tuple[str, set[str]]:
+    '''returns a list of tuple from a dictionary where the key == "name"
+    '''    
+    if type(selection) == int:
+        valid_input_list = [str(i) for i in range(1, selection + 1)]
+        prompt_end = construct_prompt_ending(valid_input_list)
+        valid_keys = set(valid_input_list)
+        return prompt_end, valid_keys
+    else:
+        valid_input_list = [f"{selection[k]['name']}" for k in selection] # pyright: ignore[reportIndexIssue, reportGeneralTypeIssues]
+        prompt_end = construct_prompt_ending(valid_input_list)
+        valid_keys = set(i[0].lower() for i in valid_input_list)
+        return prompt_end, valid_keys
 
 
 # ================================
@@ -97,6 +130,7 @@ def set_up_score_sheet(players: int) -> dict:
     score_sheet = {}
     for p in range(1, players + 1):
         score_sheet[f"Player_{p}"] = 0
+        for p in range(1, players + 1)
     score_sheet["CPU"] = 0
     return score_sheet
 
@@ -106,44 +140,51 @@ def set_up_score_sheet(players: int) -> dict:
 # ================================
 
 
-def play_new_game_choice() -> bool:
-    response = get_valid_response({"y", "n"}, "New Game? [Y]es or [N]o?: ")
-    if response == "y":
-        return True
-    else:
-        return False
-
-
 def get_players() -> int:
-    choices_with_brackets = [f"[{i}]" for i in range(1, MAX_PLAYERS + 1)]
-    
-    valid_keys = [str(i) for i in range(1, MAX_PLAYERS + 1)]
-    
-    main_text = ", ".join(choices_with_brackets[:-1])
-    options_text = f"{main_text}, or {choices_with_brackets[-1]}"
-    prompt = f"\nHow many players are playing against me (CPU)? {options_text}?: "
+    prompt_start = "\nHow many players are playing against me (CPU)?"
+    prompt_end, valid_keys = construct_prompt_and_keys(MAX_PLAYERS)
 
-    return int(get_valid_response(valid_keys, prompt))
+    response = int(get_valid_response(valid_keys, f"{prompt_start} {prompt_end}"))
+    return response
 
 
-def get_game_type(players) -> str:
+def get_game_type(players) -> str:  
     if players == 1:
-        return None
-    else:    
-        options_list = [f"[{k.upper()}]{v[1:]}" for k, v in GAME_TYPE.items()]
-        options_text = "? Or ".join(options_list)
-        prompt = f"\n{options_text}?: "
+        game_types = [f"{k}" for k in GAME_TYPE]
+        return game_types[0]
+    else:
+        prompt_start = "\nWhat kind of game would you like to play?"
+        prompt_end, valid_keys = construct_prompt_and_keys(GAME_TYPE)
 
-        return get_valid_response(GAME_TYPE, prompt)
+        response = get_valid_response(valid_keys, f"{prompt_start} {prompt_end}")
+        return response
 
 
 def get_game_size() -> int:
-    options_list = [f"[{k.upper()}]{v['name'][1:]}" for k, v in DIFFICULTIES.items()]
+    prompt_start = "\nWant something fast or challenging?"
+    prompt_end, valid_keys = construct_prompt_and_keys(DIFFICULTIES)
 
-    options_text = "? ".join(options_list)
-    prompt = f"\n{options_text}?: "
+    response = get_valid_response(valid_keys, f"{prompt_start} {prompt_end}")
+    return DIFFICULTIES[response]["wins"]
 
-    return get_valid_response(DIFFICULTIES, prompt)
+
+def play_new_game_choice() -> str:
+    prompt_start = "\nNew game?"
+    prompt_end, valid_keys = construct_prompt_and_keys(NEW_GAME_CHOICES)
+
+    response = get_valid_response(valid_keys, f"{prompt_start} {prompt_end}")
+    return response
+
+def get_game_settings():
+    new_game_choice_list = [f"{k}" for k in NEW_GAME_CHOICES]
+    new_game = play_new_game_choice()
+    if new_game == new_game_choice_list[0]:
+        players = get_players()
+        game_type = get_game_type(players)
+        wins_reqd = get_game_size()
+        return players, game_type, wins_reqd
+    else:
+        return
 
 
 # ================================
